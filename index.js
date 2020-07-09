1ea9b5ebf0fc684e5373dd8cdfaab7a8ac06b4bb
slarsendisney <s.larsendisney@gmail.com>
2020-07-09 19:33:16 +0100

    Api update

--- index.js
+++ index.js
@@ -27,18 +27,18 @@ const headers = {
   Authorization: `token ${oAuthToken}`,
 };
 
-// var cors = require("cors");
-// var allowedList = ["https://designrant.app/", "http://localhost:8000"];
-// var corsOptions = {
-//   origin: function (origin, callback) {
-//     if (allowedList.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-// };
-// app.use(cors(corsOptions));
+var cors = require("cors");
+var allowedList = ["https://designrant.app/", "http://localhost:8000"];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedList.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 
